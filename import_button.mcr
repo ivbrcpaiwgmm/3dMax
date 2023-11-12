5377bb26c5a7c7b86cee14ed042ae5cb800c58e8
ksugrvkeg23908 <135979232+ksugrvkeg23908@users.noreply.github.com>
2023-11-13 01:06:31 +0300

    first add

---
+++ import_button.mcr
@@ -0,0 +1,14 @@
+﻿macroScript CreateMyButton category:"My Scripts" buttonText:"My Import" tooltip:"Run My Python Import Script"
+(
+    rollout MyRollout "My Rollout"
+    (
+        button btn_import "Import Data" width:100 height:30
+        on btn_import pressed do
+        (
+            local scriptPath = (getDir #scripts) + "\\MyScripts\\import_data.py"
+            python.ExecuteFile scriptPath
+        )
+    )
+
+    createDialog MyRollout
+)
---
+++ import_data.py
@@ -0,0 +1,34 @@
+﻿import pymxs
+import openpyxl
+
+
+# Блок импорта данных из 3d Max
+rt = pymxs.runtime
+objects = rt.objects
+data_list: list[dict] = []
+
+for obj in objects:
+    if rt.isKindOf(obj, rt.GeometryClass):
+        obj_name: str = obj.name
+
+        obj_data: dict[str, float | str] = {
+            "name": obj_name,
+            "x_bbox": obj.max.x - obj.min.x,
+            "y_bbox": obj.max.y - obj.min.y,
+            "z_bbox": obj.max.z - obj.min.z
+        }
+
+        data_list.append(obj_data)
+
+
+# Блок записи данных в Excel файл. (для более сложных задач можно вынести в отдельный модуль и доработать)
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+
+worksheet.append(["Object name", "X bbox", "Y bbox", "Z bbox"])
+for data in data_list:
+    worksheet.append([data["name"], data['x_bbox'], data['y_bbox'], data['z_bbox']])
+
+path: str = 'C:/Users/Professional/Desktop/orlov_dmitrii_tt.xlsx'
+# Замените в path название файла на необходимое и путь к месту, где нужно создать Excel файл.
+workbook.save(path)
